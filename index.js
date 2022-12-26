4f16766dfcc9c1dc5a4d4fa72723f195d1711ccd
Shenzm72 <shenszm@gamil.com>
2022-12-26 10:09:24 +0800

    add cors

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+app.use(cors())
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Hello World!')
